305d01ba7a868a902663c5e934db2c592ee94cb3
Navid <yaa110@gmail.com>
2019-05-05 18:25:36 +0430

    [common] Add constants module to contain constans values

---
+++ common/src/constants.rs
@@ -0,0 +1,13 @@
+/// The UNIX domain socket path
+pub const SOCKET_PATH: &'static str = "/tmp/cb/sock";
+
+/// The buffer size to read from socket
+pub const BUFFER_SIZE: usize = 2048;
+
+/// The splitter to frame byte stream
+pub const SPLITTER: &[u8] = b"\r\n";
+
+lazy_static! {
+    /// The length of splitter
+    pub static ref SPLITTER_LEN: usize = SPLITTER.len();
+}
--- common/src/lib.rs
+++ common/src/lib.rs
@@ -3,6 +3,7 @@ extern crate lazy_static;
 #[macro_use]
 extern crate serde_derive;
 
+pub mod constants;
 pub mod errors;
 pub mod macros;
 pub mod message;
