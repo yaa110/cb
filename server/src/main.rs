0b9183525ae4cfa0e095fa599d293c2cfd51a74e
Navid <yaa110@gmail.com>
2019-05-06 16:57:44 +0430

    [server] store PID of server in /tmp/cb.pid

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,13 +3,19 @@ extern crate log;
 #[macro_use]
 extern crate common;
 
-use common::constants::SOCKET_PATH;
+use common::constants::{SERVER_PID, SOCKET_PATH};
 use ctrlc;
 use daemonize::Daemonize;
 use server::internal::get_user_group;
 use server::{setup_clipboard, AsyncUnix, Transmitter};
 use std::fs;
 use std::path::Path;
+
+/// Removes created files
+fn clean() {
+    let _ = fs::remove_file(Path::new(SOCKET_PATH));
+    let _ = fs::remove_file(Path::new(SERVER_PID));
+}
 
 fn main() {
     env_logger::init();
@@ -30,6 +36,7 @@ fn main() {
     let daemonize = Daemonize::new()
         .user(username.as_str())
         .group(group.as_str())
+        .pid_file(SERVER_PID)
         .umask(0o000);
 
     if let Err(e) = daemonize.start() {
@@ -37,7 +44,7 @@ fn main() {
     }
 
     if let Err(e) = ctrlc::set_handler(move || {
-        let _ = fs::remove_file(socket_path);
+        clean();
         std::process::exit(0);
     }) {
         fatal!("unable to set handler of CTRL-C signals: {}", e);
@@ -52,4 +59,6 @@ fn main() {
     if let Err(e) = AsyncUnix::new(socket_path).and_then(Transmitter::listen) {
         fatal!("unable to start transmitter: {}", e);
     }
+
+    clean();
 }
